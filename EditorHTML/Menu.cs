d0acd0e46d6c56e32803af45cf70e715c8016f66
Clebson Torres <clebson.torres@gmail.com>
2022-05-12 17:09:43 -0300

    Adicionar arquivos de projeto.

---
+++ EditorHTML/Menu.cs
@@ -0,0 +1,43 @@
+﻿namespace EditorHTML
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            DrauwScreen();
+        }
+        public static void DrauwScreen()
+        {
+            Console.Write("+");
+            for (int i = 0; i < 30; i++)
+                Console.Write("-");
+
+            Console.Write("+");
+            Console.Write("\n");
+
+
+
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 30; i++)
+                    Console.Write(" ");
+
+
+                Console.Write("|");
+                Console.Write("\n");
+            }
+
+            Console.Write("+");
+            for (int i = 0; i < 30; i++)
+                Console.Write("-");
+
+            Console.Write("+");
+            Console.Write("\n");
+        }
+    }
+}
